2d1a883dd969857752b3507ffaeec18ed62e499e
satousuguru <sgr0416st@gmail.com>
2019-06-05 20:01:28 +0900

    remake error

--- src/sgr/st/properties/PropertiesReader.java
+++ src/sgr/st/properties/PropertiesReader.java
@@ -1,7 +1,6 @@
 package sgr.st.properties;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,20 +8,12 @@ import java.util.Properties;
 public class PropertiesReader {
 	Properties properties;
 
-	public PropertiesReader(String filePath) {
+	public PropertiesReader(String filePath) throws IOException {
 		InputStream inputStream;
 		properties = new Properties();
-		try {
-			inputStream = new FileInputStream(filePath);
-			properties.load(inputStream);
-			inputStream.close();
-		} catch (FileNotFoundException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		inputStream = new FileInputStream(filePath);
+		properties.load(inputStream);
+		inputStream.close();
 	}
 
 	public String getProPerty(String key) {
