c0898a1a7a58b0bd691b0fb5c8194ea7cea053fc
satousuguru <sgr0416st@gmail.com>
2019-06-07 16:33:53 +0900

    remove Icallback, OperatableThread, ThreadOperater

--- src/sgr/st/thread/RepeatedTaskThread.java
+++ src/sgr/st/thread/RepeatedTaskThread.java
@@ -1,13 +1,22 @@
 package sgr.st.thread;
 
+/**
+ * このクラスは、繰り返し処理をするスレッドの抽象モデルです。
+ * 何らかの繰り返し期間が定まっていない何らかの繰り返し処理をする場合、このモデルを継承すると便利です。
+ * 以下の様なライフサイクルとなっています。
+ * コンストラクタ → スレッドstart → doBeforeTask() → doRepeatedTask()繰り返し → stopThread() → doAfterTask()
+ *
+ * @author satousuguru
+ *
+ */
 public abstract class RepeatedTaskThread implements Runnable{
 
 	/**
 	 * スレッドが停止状態に向かっているかどうかを示すフラグです。
-	 * 外部または内部からstopthread()び出された場合にtrueになります。
+	 * 外部または内部からstopThread()び出された場合にtrueになります。
 	 * それまではfalseです。
 	 */
-	protected boolean isStopped;
+	private boolean isStopped;
 
 	/**
 	 * パラメータの初期化を行います。流動的な初期化処理はここでは行わないでください。
@@ -30,7 +39,7 @@ public abstract class RepeatedTaskThread implements Runnable{
 
 	/**
 	 * スレッドが開始したら、まずこのメソッドを実行します。
-	 * 流動的な初期化処理はここで行ってください。
+	 * 流動的な初期化処理がある場合はこのメソッドをオーバーライドしてください。
 	 * そのような処理が必要ない場合は、何も実行しなくて構いません。
 	 */
 	protected abstract void doBeforeTask();
@@ -44,9 +53,16 @@ public abstract class RepeatedTaskThread implements Runnable{
 
 	/**
 	 * stopThread()が呼ばれ、繰り返し実行されるタスクを終了した後にこのメソッドが呼び出されます。
-	 * 後処理をここで行ってください。
+	 * 繰り返し処理が終わった後に実行したい処理がある場合はこのメソッドをオーバーライドしてください。
+	 * そのような処理が必要ない場合は、何も実行しなくて構いません。
 	 */
 	protected abstract void doAfterTask();
+
+	/**
+	 * 終了処理を行います。オブジェクトの終了処理、破棄等はここで実行してください。
+	 * そのような処理が必要ない場合は、何も実行しなくて構いません。
+	 */
+	protected abstract void close();
 
 	/**
 	 * このスレッドを停止させます。この関数を外部のスレッドから呼び出すことで、このスレッドを停止させることができます。
